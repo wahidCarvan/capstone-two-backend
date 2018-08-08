9570c78c272669363fac0db2f722f8aadb055634
wahidc7101 <wahidc7101@gmail.com>
2018-08-08 11:00:04 -0400

    clean up

--- product/router.js
+++ product/router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const router = express.router()
+const router = express.Router()
 const Product = require('./models');
 
 
